8993c893a159af9c250cef6d4bc1ed83e24dc05d
LAB 604 <lab604@SAKEC.LOCAL>
2025-08-10 11:54:15 +0530

    New

--- app1.js
+++ app1.js
@@ -1,12 +1,35 @@
-const express=require('express');
-    const app=express();
-    const port=3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-    app.get('/',(req,res)=>{
-        res.send('Hello world!');
-    })
+app.get('/', (req, res) => {
+    // console.log(`$`)
+    res.send('Hello world!');
+})
 
-    app.listen(port,()=>{
-        console.log(`Server listening at http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+}
+)
+
+// const users = [
+//     { id: 1, name: 'Alice', email: 'alice@example.com' },
+//     { id: 2, name: 'Bob', email: 'bob@example.com' },
+//     { id: 3, name: 'charlie', email: 'charlie@example.com' }
+
+// ]
+
+// app.get('/api/users', (req, res) => {
+//     console.log(`GET users was called ${req.ip}`);
+
+//     res.json(users);
+// })
+
+app.get('/api/users/:id',(req,res)=>{
+    const userid=parseInt(req.params.id);
+    const user =user.find(u=>u.id==userId);
+    if(user){res.json(user);}
+    else{
+        res.status(404).send('User not found');
     }
-    )
+})
